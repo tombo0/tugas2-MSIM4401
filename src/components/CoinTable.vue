d853a271f06ae977792cc1000665c584bd57c62b
Taufik Maulana <profesorgreen36@gmail.com>
2024-12-05 21:33:40 +0700

    chore: add styling

--- src/components/CoinTable.vue
+++ src/components/CoinTable.vue
@@ -5,15 +5,15 @@
     </ion-card-header>
     <ion-card-content>
       <ion-grid>
-        <ion-row>
+        <!-- <ion-row>
           <ion-col><strong>Name</strong></ion-col>
           <ion-col><strong>Symbol</strong></ion-col>
           <ion-col><strong>Harga USD</strong></ion-col>
-        </ion-row>
+        </ion-row> -->
         <ion-row v-for="coin in coins" :key="coin.id">
-          <ion-col>{{ coin.symbol }}</ion-col>
-          <ion-col>{{ coin.name }}</ion-col>
-          <ion-col>{{ coin.price_usd }}</ion-col>
+          <ion-col id="coin-rank"><p class="col-header">Rank</p>{{ coin.rank }}</ion-col>
+          <ion-col id="coin-symbol"><p class="col-header">{{ coin.name }}</p>{{ coin.symbol }}</ion-col>
+          <ion-col id="coin-price-usd"><p class="col-header">USD</p>{{ coin.price_usd }}</ion-col>
         </ion-row>
       </ion-grid>
     </ion-card-content>
@@ -48,5 +48,22 @@ ion-card {
 }
 ion-button {
   margin-bottom: 16px;
+}
+ion-card {
+  background-color: white;
+}
+ion-col {
+  background-color: #fef7dc;
+  text-align: center;
+  color: black;
+}
+ion-row {
+  border: 1px solid #c7c55a;
+}
+.col-header {
+  font-size: 10px;
+}
+#coin-price-usd, #coin-symbol{
+  text-align: left;
 }
 </style>
